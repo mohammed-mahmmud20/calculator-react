4b9e34511d62b8855c6239c7cf25d9622c97c9f5
mohammed-mahmmud20 <mohammedmahmmud20@gmail.com>
2022-11-21 23:01:44 +0200

    calculator-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
 import './App.css';
-
+const ACTIONS = {
+  ADO_DIGIT: 'add-didit',
+  CHOSSE_OPERATION: 'chosse-operation',
+  CLEAR:'clear',
+  DELETE_DIGIT: 'delete-didit',
+  EVALUATE: 'evaluate'
+}
+function reducer(state, { type , payload }) {
+  switch (type) {
+    case ACTIONS.ADO_DIGIT:
+      return{
+        ...state,
+        currentOperand:`$(currentOperand)$(payload.didit)`
+      }
+  }
+}
 function App() {
+  const [{currentOperand, previowsOperand, operation}, dispatch] = useReducer(reducer, {})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previows-operand">{previowsOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
+      </div>
+      <button className="span-tow">AC</button>
+      <button>DEL</button>
+      <button>/</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-tow">=</button>
     </div>
   );
 }
