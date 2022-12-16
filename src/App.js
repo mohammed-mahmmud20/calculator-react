199549cd0f0731bbe79f0234916e2a22e23930d9
mohammed mohammed <111295776+mohammed-mahmmud20@users.noreply.github.com>
2022-12-16 15:27:23 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,58 +1,8 @@
-// import { useReducer } from 'react';
-// import './App.css';
-// const ACTIONS = {
-//   ADD_DIGIT: 'add-didit',
-//   CHOSSE_OPERATION: 'chosse-operation',
-//   CLEAR:'clear',
-//   DELETE_DIGIT: 'delete-didit',
-//   EVALUATE: 'evaluate'
-// }
-// function reducer(state, { type , payload }) {
-//   switch (type) {
-//     case ACTIONS.ADD_DIGIT:
-//       return{
-//         ...state,
-//         currentOperand:`$(currentOperand)$(payload.didit)`
-//       }
-//   }
-// }
-// function App() {
-//   const [{currentOperand, previowsOperand, operation}, dispatch] = useReducer(reducer, {})
-//   dispatch ( type: ACTIONS.ADD_DIGIT)
-//   return (
-//     <div className="calculator-grid">
-//       <div className="output">
-//         <div className="previows-operand">{previowsOperand} {operation}</div>
-//         <div className="current-operand">{currentOperand}</div>
-//       </div>
-//       <button className="span-tow">AC</button>
-//       <button>DEL</button>
-//       <button>/</button>
-//       <button>1</button>
-//       <button>2</button>
-//       <button>3</button>
-//       <button>*</button>
-//       <button>4</button>
-//       <button>5</button>
-//       <button>6</button>
-//       <button>+</button>
-//       <button>7</button>
-//       <button>8</button>
-//       <button>9</button>
-//       <button>-</button>
-//       <button>.</button>
-//       <button>0</button>
-//       <button className="span-tow">=</button>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import { useReducer } from "react"
 import DigitButton from "./DigitButton"
 import OperationButton from "./OperationButton"
 import './App.css';
+
 export const ACTIONS = {
   ADD_DIGIT: "add-digit",
   CHOOSE_OPERATION: "choose-operation",
